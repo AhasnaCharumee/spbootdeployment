6657886c224d9e2364197ff7f60adba68db68d8a
AhasnaCharumee <charumeeahasna@gmail.com>
2025-08-04 11:33:52 +0530

    add config

---
+++ src/main/java/lk/ijse/gdse72/spbootdeployment/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse72.spbootdeployment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/customers/**").permitAll()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+}
