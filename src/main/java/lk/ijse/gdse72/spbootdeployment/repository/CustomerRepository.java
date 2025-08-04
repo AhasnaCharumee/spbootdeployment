a13f831201f1c581cd2f81c56bbf4edf082d5bd2
AhasnaCharumee <charumeeahasna@gmail.com>
2025-08-04 11:13:53 +0530

    update repo

--- src/main/java/lk/ijse/gdse72/spbootdeployment/repository/CustomerRepository.java
+++ src/main/java/lk/ijse/gdse72/spbootdeployment/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package lk.ijse.gdse72.spbootdeployment.repository;
 import lk.ijse.gdse72.spbootdeployment.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
