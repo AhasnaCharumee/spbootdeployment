af902bcea14dc713ab44f0c67c9308208043c25c
AhasnaCharumee <charumeeahasna@gmail.com>
2025-08-04 10:28:18 +0530

    change version

--- src/main/java/lk/ijse/gdse72/spbootdeployment/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse72/spbootdeployment/controller/CustomerController.java
@@ -1,4 +1,14 @@
 package lk.ijse.gdse72.spbootdeployment.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+    @GetMapping
+    public String getCustomers() {
+        return "List of all customers";
+    }
 }
